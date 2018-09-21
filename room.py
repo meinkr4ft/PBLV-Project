a51dfcc948beacbca689baeb3a67c4750b781d4b
l0renor <leon.lukas11@web.de>
2018-09-21 10:33:13 +0200

    sound

--- room.py
+++ room.py
@@ -7,6 +7,8 @@ class Room:
     def __init__(self):
         # You may want many lists. Lists for coins, monsters, etc.
         self.wall_list = None
+        self.hazard_list = None
+        self.enemy_list = None
 
         # This holds the background images. If you don't want changing
         # background images, you can delete this part.
